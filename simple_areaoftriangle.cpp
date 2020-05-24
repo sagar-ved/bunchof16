1da0a9a841eab82119813a710538556a015069f7
Sagar Ved Bairwa <56448072+Sagarvedbairwa@users.noreply.github.com>
2020-05-24 20:44:58 +0530

    Find Area Of Triangle
    C++ Program To Find Area Of Triangle
    
    area of triangle=(height*base)/2

---
+++ simple_areaoftriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//the area of triangle=1/2(height*base)
+	float area,height,base;
+	cout<<"Enter height and base(in cm)"<<endl;
+	cin>>height>>base;
+	
+	area=(height*base)/2;
+	
+	cout<<"area of triangle is: "<<area<<" cm^2";
+	}
