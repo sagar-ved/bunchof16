119189a06c2fd69d0cecd84be8cd3a0dc64b5303
Sagar Ved Bairwa <56448072+Sagarvedbairwa@users.noreply.github.com>
2020-05-24 22:04:39 +0530

    Check Number Is Positive Or Negative
    C++ Program To Check Number Is Positive Or Negative
    if(interger>0)
    	cout<<"you entered positive";
    	else if(interger<0)
    	cout<<"you entered negetive";
    	else
    	cout<<"you entered zero";

---
+++ simple_9_positive-or-negetive.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int interger;
+	cout<<"enter a number : ";
+	cin>>interger;
+	if(interger>0)
+	cout<<"you entered positive";
+	else if(interger<0)
+	cout<<"you entered negetive";
+	else
+	cout<<"you entered zero";
+	return 0;
+	}
