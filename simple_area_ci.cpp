aaa234bca2d6d0f170452d829da9a4418088aa91
Sagar Ved Bairwa <56448072+Sagarvedbairwa@users.noreply.github.com>
2020-05-24 20:26:03 +0530

    Find Area And Circumference Of Circle
    C++ Program To Find Area And Circumference Of Circle

---
+++ simple_area_ci.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int rad;
+float pi=3.14, area,ci;
+cout<<"enter radius of circle:"<<endl;
+cin>>rad;
+area=pi*rad*rad;
+ci=2*pi*rad;
+cout<<"area of circle="<<area<<endl<<"cirumfarence of circle="<<ci<<endl;
+
+}
